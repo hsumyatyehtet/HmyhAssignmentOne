10902c47fce34ea6fabd8dd213e9491faf068614
Hsu Myat Ye Htet <59465030+hsumyatyehtet-mmds@users.noreply.github.com>
2021-11-09 10:08:06 -0300

    navigation component set up

--- app/src/main/java/dev/hmyh/hmyhassignmentone/activity/MainActivity.kt
+++ app/src/main/java/dev/hmyh/hmyhassignmentone/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.hmyh.hmyhassignmentone
+package dev.hmyh.hmyhassignmentone.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.hmyh.hmyhassignmentone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
